a85bf6489bef3b2813db642b2fad45ea3406f82d
rgaschel <72839296+rgaschel@users.noreply.github.com>
2021-02-22 20:13:17 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,137 @@
+window.alert("Hello, and welcome to my site.  This website allows users to select their favorite locations on a map and enter useful information about that place.  If you'd like, select your favorite location in Castleton-on-Hudson, and let us know what it's called and what you like about it, this map allows you to select general areas by drawing a polygon, so whether it be your favorite bar, or favorite park, we have you covered!");
+
+var map = L.map('map').setView([42.53573350982836, -73.74984925036651], 15);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoicmdhc2NoZWwiLCJhIjoiY2tsNnloNGN4MWgwMTJvbnJmMXlkbnRqdiJ9.w55psDu7ynDSCh0APKt6Rw'
+}).addTo(map);
+
+var drawnItems = L.featureGroup().addTo(map);
+
+var cartoData = L.layerGroup().addTo(map);
+var url = "https://rgaschel.carto.com/api/v2/sql";
+var urlGeoJSON = url + "?format=GeoJSON&q=";
+var sqlQuery = "SELECT the_geom, description, name FROM lab_3b_gaschel";
+function addPopup(feature, layer) {
+    layer.bindPopup(
+        "<b>" + feature.properties.name + "</b><br>" +
+        feature.properties.description
+    );
+}
+
+fetch(urlGeoJSON + sqlQuery)
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data, {onEachFeature: addPopup}).addTo(cartoData);
+    });
+
+new L.Control.Draw({
+    draw : {
+        polygon : true,
+        polyline : false,      //polylines disabled
+        rectangle : false,     // Rectangles disabled
+        circle : false,        // Circles disabled
+        circlemarker : false,  // Circle markers disabled
+        marker: true
+    },
+    edit : {
+        featureGroup: drawnItems
+    }
+}).addTo(map);
+
+function createFormPopup() {
+    var popupContent =
+        '<form>' +
+        'Title:<br><input type="text" id="input_name"><br>' +
+        'Description:<br><input type="text" id="input_desc"><br>' +
+        '<input type="button" value="Submit" id="submit">'
+        '</form>'
+    drawnItems.bindPopup(popupContent).openPopup();
+}
+
+map.addEventListener("draw:created", function(e) {
+    e.layer.addTo(drawnItems);
+    drawnItems.eachLayer(function(layer) {
+        var geojson = JSON.stringify(layer.toGeoJSON().geometry);
+        console.log(geojson);
+        createFormPopup();
+    });
+});
+
+function setData(e) {
+
+    if(e.target && e.target.id == "submit") {
+
+        // Get user name and description
+        var enteredUsername = document.getElementById("input_name").value;
+        var enteredDescription = document.getElementById("input_desc").value;
+
+        // For each drawn layer
+      drawnItems.eachLayer(function(layer) {
+
+  			// Create SQL expression to insert layer
+              var drawing = JSON.stringify(layer.toGeoJSON().geometry);
+              var sql =
+                  "INSERT INTO lab_3b_gaschel (the_geom, name, description) " +
+                  "VALUES (ST_SetSRID(ST_GeomFromGeoJSON('" +
+                  drawing + "'), 4326), '" +
+                  enteredUsername + "', '" +
+                  enteredDescription + "')";
+              console.log(sql);
+
+              // Send the data
+              fetch(url, {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/x-www-form-urlencoded"
+                  },
+                  body: "q=" + encodeURI(sql)
+              })
+              .then(function(response) {
+                  return response.json();
+              })
+              .then(function(data) {
+                  console.log("Data saved:", data);
+              })
+              .catch(function(error) {
+                  console.log("Problem saving the data:", error);
+              });
+
+          // Transfer submitted drawing to the CARTO layer
+          //so it persists on the map without you having to refresh the page
+          var newData = layer.toGeoJSON();
+          newData.properties.description = enteredDescription;
+          newData.properties.name = enteredUsername;
+          L.geoJSON(newData, {onEachFeature: addPopup}).addTo(cartoData);
+
+      });
+
+        // Clear drawn items layer
+        drawnItems.closePopup();
+        drawnItems.clearLayers();
+
+    }
+}
+
+document.addEventListener("click", setData);
+
+map.addEventListener("draw:editstart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:deletestart", function(e) {
+    drawnItems.closePopup();
+});
+map.addEventListener("draw:editstop", function(e) {
+    drawnItems.openPopup();
+});
+map.addEventListener("draw:deletestop", function(e) {
+    if(drawnItems.getLayers().length > 0) {
+        drawnItems.openPopup();
+    }
+});
